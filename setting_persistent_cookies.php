d08c50e752f53d1966bd865ca40d125ad598491e
MohdFazly <2020819344@student.uitm.edu.my>
2022-06-06 09:42:25 +0800

    commit 2

--- setting_persistent_cookies.php
+++ setting_persistent_cookies.php
@@ -4,25 +4,25 @@ setcookie ("myLoginName", "myWebsite");
 setcookie ("myPreferredColor", "Blue");
 setcookie ("CouponNumber", "07470433", time() +60*60*24*7); setcookie ("CouponValue", "100.00", time () +60*60*24*7);
 
-print ("\n");
-print ("2 temporary cookies were delivered.\n"); 
-print ("2 consistent cookies were delivered.\n");
+print ("<br>");
+print ("2 temporary cookies were delivered.<br>"); 
+print ("2 consistent cookies were delivered.<br>");
 
 if (isset($_COOKIE ["myLoginName"])) 
 {
     $loginName = $_COOKIE ["myLoginName"];
-    print("Received a cookie named as myLoginName: ".$loginName."\n"); 
+    print("Received a cookie named as myLoginName: ".$loginName."<br>"); 
 } else 
 { 
-    print("Did not received any cookie named as myLoginName.\n");
+    print("Did not received any cookie named as myLoginName.<br>");
 }
 
 $count = count ($_COOKIE);
-print ("Scount cookies received.\n"); 
+print ("Scount cookies received.<br>"); 
 foreach ($_COOKIE as $name => $value)
 { 
-    print " $name = $value\n";
+    print " $name = $value<br>";
 }
-print ("\n");
+print ("<br>");
 
 ?>
