fcc7e3062a7c86b75b48501f1db50a439446db21
MohdFazly <2020819344@student.uitm.edu.my>
2022-06-06 09:29:31 +0800

    commit 2

--- setting_persistent_cookies.php
+++ setting_persistent_cookies.php
@@ -1,24 +1,28 @@
 <?php
-setcookie ("myLoginName", "MyWebsite");
+
+setcookie ("myLoginName", "myWebsite");
 setcookie ("myPreferredColor", "Blue");
-echo "<br>";
-echo "2 cookies were delivered.<br>";
+setcookie ("CouponNumber", "07470433", time() +60*60*24*7); setcookie ("CouponValue", "100.00", time () +60*60*24*7);
 
-if (isset($_COOKIE ["myLoginName"]))
-{ 
-    $loginName = $_COOKIE ["myLoginName"];
-    echo "Received a cookie named as LoginName: ".$loginName."<br>";
-} else
+print ("\n");
+print ("2 temporary cookies were delivered.\n"); 
+print ("2 consistent cookies were delivered.\n");
+
+if (isset($_COOKIE ["myLoginName"])) 
 {
-    echo "Did not received any cookie named as LoginName.<br>";
+    $loginName = $_COOKIE ["myLoginName"];
+    print("Received a cookie named as myLoginName: ".$loginName."\n"); 
+} else 
+{ 
+    print("Did not received any cookie named as myLoginName.\n");
 }
 
-$count count ($_COOKIE);
-echo "$count cookies received.<br>");
-foreach ($_COOKIE as $name => $value) 
+$count = count ($_COOKIE);
+print ("Scount cookies received.\n"); 
+foreach ($_COOKIE as $name => $value)
 { 
-    echo " $name = $value<br>";
+    print " $name = $value\n";
 }
-echo "<br>");
+print ("\n");
 
 ?>
