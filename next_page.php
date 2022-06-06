facabfd7af0f1f4ee10f46d9f3bb76d480b91519
MohdFazly <2020819344@student.uitm.edu.my>
2022-06-06 08:29:43 +0800

    commit 3

--- next_page.php
+++ next_page.php
@@ -2,6 +2,13 @@
 session_start();
 echo "<html>";
 
+
+$sid = session id();
+echo "Session ID returned by session_id(): ".$sid. "\n";
+$sid = SID;
+echo "Session ID returned by SID: ".$sid. "\n";
+
+
 $myLogin = $_SESSION["myLogin"];
 echo "Value of myLogin has been retrieved: ".$myLogin. "\n";
 
