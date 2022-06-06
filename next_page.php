3f2cc929f971faed763dca2ab339e3ae9c29aee4
MohdFazly <2020819344@student.uitm.edu.my>
2022-06-06 08:41:16 +0800

    commit 3

--- next_page.php
+++ next_page.php
@@ -4,16 +4,16 @@ echo "<html>";
 
 
 $sid = session_id();
-echo "Session ID returned by session_id(): ".$sid. "\n";
+echo "Session ID returned by session_id(): ".$sid. "<br>";
 $sid = SID;
-echo "Session ID returned by SID: ".$sid. "\n";
+echo "Session ID returned by SID: ".$sid. "<br>";
 
 
 $myLogin = $_SESSION["myLogin"];
-echo "Value of myLogin has been retrieved: ".$myLogin. "\n";
+echo "Value of myLogin has been retrieved: ".$myLogin. "<br>";
 
 $myColor = $_SESSION["myColor"];
-echo "Value of MyColor has been retrieved: ".$myColor."\n";
+echo "Value of MyColor has been retrieved: ".$myColor."<br>";
 
 echo "</html>\n";
 
